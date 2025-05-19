98e0b20da84cdee8aac61d88243fd5f7c98bf5db
sxy15 <sxy_19921115@163.com>
2025-05-19 21:37:53 +0800

    refactor: mapProps

--- packages/utils/src/map.ts
+++ packages/utils/src/map.ts
@@ -1,20 +1,23 @@
 interface Item {
-    name: string;
-    defaultValue?: any; // 若 defaultValue 有明确类型可替换为具体类型
-    [key: string]: any;
+	name: string;
+	defaultValue?: any; // 若 defaultValue 有明确类型可替换为具体类型
+	[key: string]: any;
 }
 
 interface UIConfigItem {
-    items: Item[];
-    [key: string]: any;
+	items: Item[];
+	[key: string]: any;
 }
 
 export const mapProps = (uiConfig: UIConfigItem[]) => {
-    return uiConfig
-        .map((it: UIConfigItem) => it.items)
-        .flat()
-        .reduce((prev: Record<string, any>, current: Item) => {
-            prev[current.name] = current.defaultValue ?? null;
-            return prev;
-        }, {} as Record<string, any>);
+	return uiConfig
+		.map((it: UIConfigItem) => it.items)
+		.flat()
+		.reduce(
+			(prev: Record<string, any>, current: Item) => {
+				prev[current.name] = current.defaultValue ?? null;
+				return prev;
+			},
+			{} as Record<string, any>,
+		);
 };
