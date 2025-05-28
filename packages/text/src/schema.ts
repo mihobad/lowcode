a5db8b9ce54585a6db166952e9191a8e68993843
sxy15 <sxy_19921115@163.com>
2025-05-28 14:51:10 +0800

    refactor: version

--- packages/text/src/schema.ts
+++ packages/text/src/schema.ts
@@ -1,10 +1,11 @@
 // Schema 扩展默认值相关处理
 import propConfig from './merchant/prop-config/index';
 import { mapProps } from '@anfu/utils';
+import { version } from '../package.json';
 
 const textSchemaJson = {
 	name: 'text',
-	version: '1.0.0',
+	version: version,
 	description: 'text',
 	props: mapProps(propConfig),
 	data: {},
